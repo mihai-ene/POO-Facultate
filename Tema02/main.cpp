da78d8bae2eb5201206fd06038a40bb941fa2fd9
Mihai Ene <57237755+mihai-ene@users.noreply.github.com>
2020-12-15 23:43:09 +0200

    Update main.cpp

--- Tema02/main.cpp
+++ Tema02/main.cpp
@@ -1,23 +1,52 @@
-#include <memory>
-#include "masina_electrica.h"
+#include <iostream>
 
-int main() {
-    std::unique_ptr<motor> TestMotorException(new motor("",0,0.5));
-    std::unique_ptr<motor> motor1(new motor("V12",300,2.2));
+class motor{
+private:
+    std::string denumire;
+    int putere;
+    float consum;
+public:
+    motor(std::string, int, float);
+    ~motor();
+    std::string getDenumire() const;
+    int getPutere() const;
+    float getConsum() const;
+    motor(const motor&);
+};
 
-    std::unique_ptr<masina> TestMasinaException(new masina(*motor1,"","","",0));
+motor::motor(std::string denumire, int putere, float consum){
+    try{
+        if (denumire !="" && putere>=50 && consum >= 1){
+            this->denumire = denumire;
+            this->putere = putere;
+            this->consum = consum;
+        }else{
+            throw 1;
+        }
+    }
+    catch (int Error){
+        std::cout<<"Error code: "<<Error<<"\n"
+                 <<"- Denumirea motorului nu poate fi nula"<<"\n"
+                 <<"- Puterea motorului nu poate fi mai mica ca 50 de cai putere"<<"\n"
+                 <<"- Consumul motorului nu poate fi mai mic ca 1%"<<"\n";
+    }
 
-    std::unique_ptr<masina> Dacie(new masina(*motor1,"Dacie","Duster","Rosie",5));
+};
+motor::motor(const motor &other){
+    this->denumire = other.denumire;
+    this->putere = other.putere;
+    this->consum = other.consum;
+}
+std::string motor::getDenumire() const{
+    return this->denumire;
+}
+float motor::getConsum() const {
+    return this->consum;
+}
+int motor::getPutere() const {
+    return this->putere;
+}
 
-    std::unique_ptr<motor> motor_electric(new motor("Tesla Motor V2",600,200));
-    std::unique_ptr<masina_electrica> tesla(new masina_electrica(*motor_electric, "Tesla","Model S", "Neagra",4,2000));
-    tesla->print();
-    Dacie->print();
-
-
-    tesla->sunet();
-    Dacie->sunet();
-
-
-    return 0;
+motor::~motor(){
+    std::cout << "Am distrus motorul " << this->denumire <<"\n";
 }
