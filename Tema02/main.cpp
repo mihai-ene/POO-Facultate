00e4f4c8fcb7ace5e0c2c7d14d19642d4376ba16
Mihai Ene <57237755+mihai-ene@users.noreply.github.com>
2020-12-17 17:26:53 +0200

    Update main.cpp

--- Tema02/main.cpp
+++ Tema02/main.cpp
@@ -5,18 +5,18 @@ int main() {
     std::unique_ptr<motor> motor1 = std::make_unique<motor>("V12",300,2.2);
 
     //PENTRU TESTAREA EXCEPTIEI DIN CONSTRUCTOR:
-   /* try {
-        std::unique_ptr<motor> motor_invalid = std::make_unique<motor>("", 0, 0.5);
-    }
-        catch(std::exception&e){
-            std::cout<<e.what();
-        }
-    try {
-        std::unique_ptr<masina> masina_invalida = std::make_unique<masina>(*motor1,"","","",0,"",0,0);
-    }
-        catch(std::exception &ex){
-            std::cout<<ex.what();
-        } */
+    /* try {
+         std::unique_ptr<motor> motor_invalid = std::make_unique<motor>("", 0, 0.5);
+     }
+         catch(std::exception&e){
+             std::cout<<e.what();
+         }
+     try {
+         std::unique_ptr<masina> masina_invalida = std::make_unique<masina>(*motor1,"","","",0,"",0,0);
+     }
+         catch(std::exception &ex){
+             std::cout<<ex.what();
+         } */
     std::unique_ptr<masina> Dacie = std::make_unique<masina>(*motor1,"Dacie","Duster","Rosie",5,"manuala",5,180);
     std::unique_ptr<masina> BMW = std::make_unique<masina>(*motor1,"BMW","M4","Neagra",4,"automata",7,250);
 
@@ -29,6 +29,7 @@ int main() {
     //std::cout<<*Dacie;
     //std::cout<<*tesla;
 
+    // TESTARE FUNCTIE VIRTUALA
     //tesla->sunet();
     //Dacie->sunet();
 
