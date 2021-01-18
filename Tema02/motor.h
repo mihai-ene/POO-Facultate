9544d7d2660cbac4b2e84b671bb8d858bd49b2bf
Mihai Ene <57237755+mihai-ene@users.noreply.github.com>
2021-01-18 12:23:55 +0200

    Adaugare cerinte tema 3

--- Tema02/motor.h
+++ Tema02/motor.h
@@ -7,10 +7,12 @@ private:
     float consum;
 public:
     motor(std::string, int, float);
+    motor();
     ~motor();
     std::string getDenumire() const;
     int getPutere() const;
     float getConsum() const;
+    friend std::ostream& operator<<(std::ostream& os, const motor& m);
     motor(const motor&);
     motor& operator=(const motor &other){
         this->denumire = other.denumire;
